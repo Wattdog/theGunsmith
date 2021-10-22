159c68d06c3d86d4a9ac0aa9dd96393461931e68
Adam Hurst (s5226058) <s5226058@bournemouth.ac.uk>
2021-10-22 19:30:07 +0100

    added basic player movement and bullet framework

---
+++ theGunsmith_GAMEJAM/Assets/Scripts/bulletController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletController : MonoBehaviour
+{
+    
+    /*
+        -Create bullet
+        -Calculate unit vector from orgin to mouse location
+        -multiply result by bullet speed * delta time
+        -push bullet every frame by this vector
+        -delete afeter out of scene
+         
+         */
+
+    public float bulletSpeed = 5.0f;
+    public Vector3 direction = new Vector3();
+
+    private bool bulletGo = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bulletGo)
+        {
+            transform.position += direction * Time.deltaTime; 
+        }
+    }
+
+    public void startBullet()
+    {
+        bulletGo = true;
+    }
+    public void setupBullet(Vector2 currentLoc, Vector2 mouseLoc)
+    {
+        Vector3 unitVec = (mouseLoc - currentLoc).normalized;
+        direction = unitVec;
+
+        direction *= bulletSpeed;
+    }
+
+
+}
---
+++ theGunsmith_GAMEJAM/Assets/Scripts/playerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    [SerializeField] public float moveSpeed;
+    [SerializeField] public int health;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 
+
+        Vector3 move = new Vector3();
+        if (Input.GetKey(KeyCode.A))
+        {
+            move.x -= moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            move.x += moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            move.y += moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            move.y -= moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            
+        }
+
+        GetComponent<Transform>().position = GetComponent<Transform>().position += move;
+
+    }
+}
