288665a316c57bb719420672667106406366896c
Neil Tubbs-Bates (s5220623) <s5220623@bournemouth.ac.uk>
2021-10-23 18:11:25 +0100

    more menu shite

---
+++ theGunsmith_GAMEJAM/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool Paused = false;
+
+    public GameObject pauseUI;
+ 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        Paused = false;
+    }
+
+    private void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        Paused = true;
+    }
+}
