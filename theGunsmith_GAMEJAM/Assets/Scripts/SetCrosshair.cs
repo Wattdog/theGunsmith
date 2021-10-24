fa937ee554aa85049f3e136998a95dd6442a1f59
Aaron Harrison-Watt (s5213004) <s5213004@bournemouth.ac.uk>
2021-10-24 14:27:56 +0100

    Set crosshair script

---
+++ theGunsmith_GAMEJAM/Assets/Scripts/SetCrosshair.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCrosshair : MonoBehaviour
+{
+    public Texture2D crosshair;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Sets cursor to crosshair texture
+        Cursor.SetCursor(crosshair, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+}
